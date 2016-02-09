6590608375db567844d07f59bb19b4324088c830
Torben Dohrn <github.com@nexusger.de>
2016-02-09 23:10:48 +0100

    added draft of github releases tool

---
+++ Get-Latest-GitHub-Release.ps1
@@ -0,0 +1,22 @@
+﻿function Create-API-Url ($userName,$repo){
+    $apiUrl="https://api.github.com/repos/$userName/$repo/releases";
+    "$apiUrl"
+}
+
+function Remove-Prereleases ($JsonResponse){
+    $JsonResponse | Where-Object { $_.prerelease -eq $false } 
+}
+
+function Request-Url-Of-Latest-Version-From-Api($apiUrl){
+    $webResponse = Invoke-WebRequest -Uri $ApiUrl
+
+    $JsonResponse = ConvertFrom-Json $webResponse
+
+    $releases = Remove-Prereleases $JsonResponse
+
+    $releases[0].zipball_url
+}
+
+$apiUrl = Create-API-Url "adobe-fonts" "source-code-pro"
+
+$releaseUrl = Request-Url-Of-Latest-Version-From-Api $apiUrl
